139cfa3480229eb9048f48d7f7c80da6a6a13d08
petsagouris <petsagouris@gmail.com>
2025-04-21 17:49:24 +0300

    fix bare-except (E722)

--- django_ledger/views/bill.py
+++ django_ledger/views/bill.py
@@ -81,7 +81,7 @@ class BillModelCreateView(BillModelModelBaseView, CreateView):
             if po_item_uuids_qry_param:
                 try:
                     po_item_uuids = po_item_uuids_qry_param.split(',')
-                except:
+                except Exception:
                     return HttpResponseBadRequest()
             else:
                 return HttpResponseBadRequest()
